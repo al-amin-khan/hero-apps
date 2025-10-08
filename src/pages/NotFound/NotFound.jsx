5718755c34fb189d661bfff155e8f40b3a57d161
Al Amin Khan <alaminkhan2010@gmail.com>
2025-10-08 07:05:30 +0600

    fix: change in UI for app and error page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -5,15 +5,16 @@ const NotFound = () => {
     const navigate = useNavigate();
     return (
         <div className='text-center my-10'>
-            <div className='flex flex-col justify-center items-center my-4'>
-                <img className='w-[40%] h-full' src="../../src/assets/error-404.png" alt="404 Not Found" />
-                <p className='text-2xl text-gray-400'>The page you are looking for does not exist.</p>
+            <div className='flex flex-col justify-center items-center my-4 space-y-2'>
+                <img className='h-full' src="../src/assets/error-404.png" alt="404 Not Found" />
+                <p className='text-5xl text-gray-700'>Oops, page not found</p>
+                <p className='text-xl text-gray-400'>The page you are looking for does not available.</p>
                 <button 
-                className='btn btn-soft mt-5 flex justify-center items-center'
+                className='btn bg-linear-[125deg,#632EE3,#9F62F2] text-white mt-5 flex justify-center items-center'
                 onClick={() => {navigate(-1)}}
                 >
                     <MdArrowBackIos className='font-bold mt-0.5' />
-                    <span>Back To Previous Page</span>
+                    Go Back!
                 </button>
             </div>
         </div>
