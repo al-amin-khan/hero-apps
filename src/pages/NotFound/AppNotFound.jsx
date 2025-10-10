de1ba91fe6bd8b098d68580c405d2d00129dacfd
Al Amin Khan <alaminkhan2010@gmail.com>
2025-10-10 18:31:26 +0600

    fix: app not found and page not found error image

--- src/pages/NotFound/AppNotFound.jsx
+++ src/pages/NotFound/AppNotFound.jsx
@@ -1,12 +1,13 @@
 import { MdArrowBackIos } from 'react-icons/md';
 import { useNavigate } from 'react-router';
+import appErrorImage from '../../assets/App-Error.png';
 
 const AppNotFound = () => {
     const navigate = useNavigate();
     return (
         <div className='text-center my-10'>
             <div className='flex flex-col justify-center items-center my-4 space-y-2'>
-                <img className='h-full' src="../src/assets/App-Error.png" alt="404 Not Found" />
+                <img className='h-full' src={appErrorImage} alt="404 Not Found" />
                 <p className='text-4xl text-gray-700'>Oops, The App Not Found</p>
                 <p className='text-lg text-gray-400'>The app you are looking for does not available.</p>
                 <button
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,13 @@
 import { MdArrowBackIos } from 'react-icons/md';
 import { useNavigate } from 'react-router';
+import errorImage from '../../assets/error-404.png'
 
 const NotFound = () => {
     const navigate = useNavigate();
     return (
         <div className='text-center my-10'>
             <div className='flex flex-col justify-center items-center my-4 space-y-2'>
-                <img className='h-full' src="../src/assets/error-404.png" alt="404 Not Found" />
+                <img className='h-full' src={errorImage} alt="404 Not Found" />
                 <p className='text-5xl text-gray-700'>Oops, page not found</p>
                 <p className='text-xl text-gray-400'>The page you are looking for does not available.</p>
                 <button 
